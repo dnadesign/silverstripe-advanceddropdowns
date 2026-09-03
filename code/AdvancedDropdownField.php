a9c802d681b50c47c58083ba9de8e66564b266e1
Avigail <avigail.lim@dna.co.nz>
2026-09-03 21:39:41 +1200

    Upgrade code for SS6

--- code/AdvancedDropdownField.php
+++ code/AdvancedDropdownField.php
@@ -2,92 +2,87 @@
 
 namespace DNA\AdvancedDropdowns;
 
-
+use SilverStripe\Model\ArrayData;
+use SilverStripe\Model\List\ArrayList;
 use SilverStripe\Forms\DropdownField;
-use SilverStripe\View\ArrayData;
-use SilverStripe\ORM\ArrayList;
 use SilverStripe\Forms\FormField;
 
+class AdvancedDropdownField extends DropdownField
+{
+    protected $extraClasses = ['dropdown advanceddropdown'];
 
-
-class AdvancedDropdownField extends DropdownField {
-
-	protected $extraClasses = array('dropdown advanceddropdown');
-
-
-	public function Field($properties = array()) {
+	public function Field($properties = [])
+    {
 		$source = $this->getSource();
-		$options = array();
-        
+		$options = [];
+
         if ($this->getHasEmptyDefault()) {
-			$selected = ($this->value === '' || $this->value === null);
-			$disabled = (in_array('', $this->disabledItems, true)) ? 'disabled' : false;
+            $value = $this->getValue();
+            $selected = $value === '' || $value === null;
+			$disabled = $this->isDisabledValue('') ? 'disabled' : false;
 			$empty = $this->getEmptyString();
-			$options[] = new ArrayData(array(
+			$options[] = new ArrayData([
 				'Value' => '',
 				'Title' => $empty,
 				'Selected' => $selected,
 				'Disabled' => $disabled,
 				'Attributes' => $this->createOptionAttributes($empty)
-			));
+			]);
 		}
-        
+
 		if($source) {
 			foreach($source as $value => $params) {
 
-
 				$selected = false;
-				if($value === '' && ($this->value === '' || $this->value === null)) {
+                $fieldValue = $this->getValue();
+				if($value === '' && ($fieldValue === '' || $fieldValue === null)) {
 					$selected = true;
 				} else {
 					// check against value, fallback to a type check comparison when !value
 					if($value) {
-						$selected = ($value == $this->value);
+                        $selected = ($value == $fieldValue);
 					} else {
-						$selected = ($value === $this->value) || (((string) $value) === ((string) $this->value));
+                        $selected = ($value === $fieldValue) || (((string) $value) === ((string) $fieldValue));
 					}
-
-					$this->isSelected = $selected;
 				}
-				
+
 				$disabled = false;
-				if(in_array($value, $this->disabledItems) && $params['Title'] != $this->emptyString ){
+				if($this->isDisabledValue($value) && $params['Title'] != $this->getEmptyString() ){
 					$disabled = 'disabled';
 				}
-				
-				$options[] = new ArrayData(array(
+
+				$options[] = new ArrayData([
 					'Title' => $params['Title'],
 					'Value' => $value,
 					'Selected' => $selected,
 					'Disabled' => $disabled,
 					'Attributes' => $this->createOptionAttributes($params)
-				));
+				]);
 			}
 		}
 
-		$properties = array_merge($properties, array('Options' => new ArrayList($options)));
+		$properties = array_merge($properties, ['Options' => new ArrayList($options)]);
 
 		return FormField::Field($properties);
 	}
 
-
-	public function createOptionAttributes($params) {
+	public function createOptionAttributes($params)
+    {
 		$attributes = new ArrayList();
 		if(isset($params['Attributes'])) {
 			if($params['Attributes'] instanceOf ArrayList) {
 				$attributes = $params['Attributes'];
 			} else {
 				foreach($params['Attributes'] as $k => $v) {
-					$attributes->push(new ArrayData(array(
+					$attributes->push(new ArrayData([
 						'Name' => $k,
 						'Value' => $v
-					)));
+					]));
 				}
 			}
 		}
 		return $attributes;
 	}
-
 }
 
 
